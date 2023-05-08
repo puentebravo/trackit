0456c1c6cdff35c0c81fe0cc9918f7c05de3a3db
Puentebravo <puentematos@gmail.com>
2023-05-08 13:35:46 -0400

    Wrote tests to cover server, current API routes

--- __tests__/apiRoutes.test.ts
+++ __tests__/apiRoutes.test.ts
@@ -1,4 +1,4 @@
-import e from "express";
+
 import request from "supertest";
 
 const baseUrl = "http://localhost:3001"
@@ -13,11 +13,12 @@ describe("api route tests", () => {
     test("/rep should return a 200 status code", async () => {
         const response = await request(baseUrl).post("/api/rep").send({
             reps_completed: 5,
-            userId: "643f4044ca7b932abdf9fb1c",
-            workoutName: "Squats"
+            workoutName: "Squats",
+            userId: "643f4044ca7b932abdf9fb1c"
         })
 
         expect(response.statusCode).toBe(200);
     })
+
 })
 
--- __tests__/server.test.ts
+++ __tests__/server.test.ts
@@ -1,8 +1,12 @@
+import request from "supertest";
 
+const baseUrl = "http://localhost:3001"
 
 describe("server.ts tests", () => {
-    test("Can this do math", () => {
-        expect(4 + 2).toBe(6)
+    test("Is this thing on", async () => {
+        const response = await request(baseUrl).get("/api/test")
+
+        expect(response.statusCode).toBe(200);
     })
     
 })
