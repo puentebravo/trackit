3daa4e81ca92f7561f6881870e675e76a7cc562d
Puentebravo <puentematos@gmail.com>
2023-05-10 14:29:46 -0400

    Added session support

--- server.ts
+++ server.ts
@@ -2,7 +2,28 @@ const express = require("express");
 const PORT = process.env.port || 3001;
 const app = express();
 const routes = require("./routes/apiRoutes");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
 
+require("dotenv").config();
+
+
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    saveUninitialized: false,
+    resave: false,
+    store: MongoStore.create({
+      mongoUrl: process.env.DATABASE_URL,
+      autoRemove: "native",
+      touchAfter: 24 * 3600,
+      crypto: {
+        secret: process.env.ENCRYPT_SECRET,
+      },
+    }),
+  })
+);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
