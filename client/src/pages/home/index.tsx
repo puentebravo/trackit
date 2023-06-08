1d3c4022305fc8b41489c06405fd604ee58cffbb
Puentebravo <puentematos@gmail.com>
2023-06-08 19:49:31 -0400

    Scaffolded out pages

---
+++ client/src/pages/home/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+    return (
+        <header>
+            <h1>Homepage reached.</h1>
+        </header>
+    )
+}
+
+export default Home;
---
+++ client/src/pages/landing/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Landing() {
+    return (
+        <header>
+            <h1>Landing page reached.</h1>
+        </header>
+    )
+}
+
+export default Landing;
