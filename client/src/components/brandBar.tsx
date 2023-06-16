f1b7ff659f11b273689c307d79f7fbce93d63f01
Puentebravo <puentematos@gmail.com>
2023-06-15 21:23:26 -0400

    Added brandbar header component

---
+++ client/src/components/brandBar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function BrandBar() {
+
+    return (
+        <header>
+            <h1 id="brandHeader">.solid</h1>
+        </header>
+    )
+}
+
+export default BrandBar
