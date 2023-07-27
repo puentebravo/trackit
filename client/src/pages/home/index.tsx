f6a8eaa5a18a6560ad62180c39acb55f1f6fde5b
Puentebravo <puentematos@gmail.com>
2023-07-26 21:31:12 -0400

    Added formik, form input to page

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -1,8 +1,28 @@
 import React, { useEffect, useState } from "react";
 import BrandBar from "../../components/brandBar";
-import { response } from "express";
+import { useFormik } from "formik"
+import "./home.css"
 
 function Home() {
+
+    const formik = useFormik({
+        initialValues: {
+            reps: ""
+        },
+        onSubmit: (values) => {
+            console.log(values)
+            fetch("/api/rep", {
+                method: "POST",
+                body: JSON.stringify(values),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            }).then(response => {
+                console.log(response)
+
+            })
+        }
+    })
 
     const [workout, setWorkout] = useState<String>("")
 
@@ -15,7 +35,23 @@ function Home() {
     return (
         <>
             <BrandBar />
-            
+
+            <h1 id="repsHeader">{`Do ${workout}`}</h1>
+            <section id="formContainer">
+                <form id="repsForm" onSubmit={formik.handleSubmit}>
+                    <input
+                        type="text"
+                        name="reps"
+                        id="reps"
+                        className="formField"
+                        onChange={formik.handleChange}
+                        value={formik.values.reps}
+                        placeholder="How many did you do?"
+                    />
+                    <button type="submit" id="submitBtn">Send it</button>
+
+                </form>
+            </section>
 
         </>
     )
