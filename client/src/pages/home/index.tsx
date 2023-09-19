e83098022367f0f40c4e30be46027965a618df4c
Puentebravo <puentematos@gmail.com>
2023-09-19 19:56:45 -0400

    Added form for minutes

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -1,11 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import BrandBar from "../../components/brandBar";
 import { useFormik } from "formik"
 import "./home.css"
 
 function Home() {
+    const [workout, setWorkout] = useState<String>("")
 
-    const formik = useFormik({
+    const repForm = useFormik({
         initialValues: {
             reps: ""
         },
@@ -24,28 +25,55 @@ function Home() {
         }
     })
 
-    const [workout, setWorkout] = useState<String>("")
+    const minForm = useFormik({
+        initialValues: {
+            minutes: ""
+        },
+        onSubmit: (values) => {
+            fetch(`/api/workout/${values.minutes}`)
+                .then(response => response.json())
+                .then(data => {
+                    setWorkout(data.workout)
+                })
+        }
+    })
 
-    useEffect(() => {
-        fetch("/api/workout").then(response => response.json()).then(data => {
-            setWorkout(data.workout)
-        })
-    }, [])
+
+
+
+
 
     return (
         <>
             <BrandBar />
 
-            <h1 id="repsHeader">{`Do ${workout}`}</h1>
+            <section id="minFormContainer">
+                <h3 id="minHeader">How much time do you have?</h3>
+                <form id="repsForm" onSubmit={minForm.handleSubmit}>
+                    <input
+                        type="text"
+                        name="minutes"
+                        id="minutes"
+                        className="formField"
+                        onChange={minForm.handleChange}
+                        value={minForm.values.minutes}
+                        placeholder="I have this many minutes to workout"
+                    />
+                    <button type="submit" id="minuteBtn">Give me a workout!</button>
+
+                </form>
+            </section>
+
             <section id="formContainer">
-                <form id="repsForm" onSubmit={formik.handleSubmit}>
+                <h3 id="repsHeader">{`Do ${workout}`}</h3>
+                <form id="repsForm" onSubmit={repForm.handleSubmit}>
                     <input
                         type="text"
                         name="reps"
                         id="reps"
                         className="formField"
-                        onChange={formik.handleChange}
-                        value={formik.values.reps}
+                        onChange={repForm.handleChange}
+                        value={repForm.values.reps}
                         placeholder="How many did you do?"
                     />
                     <button type="submit" id="submitBtn">Send it</button>
