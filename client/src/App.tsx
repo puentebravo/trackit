f84ed58c9d1cc7a47a5fb84097a6db5bc3c3e387
Puentebravo <puentematos@gmail.com>
2023-11-14 14:45:28 -0500

    Added route for signup page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,16 +5,18 @@ import './App.css';
 import Landing from './pages/landing';
 import Login from './pages/login';
 import Home from './pages/home';
+import Signup from './pages/signup';
 
 function App() {
   return (
-   <Router basename={`${process.env.PUBLIC_URL}`}>
-    <Routes>
-     <Route path='/' element={<Landing />} />
-     <Route path='/login' element={<Login />} />
-     <Route path='/home' element={<Home />} />
-    </Routes>
-   </Router>
+    <Router basename={`${process.env.PUBLIC_URL}`}>
+      <Routes>
+        <Route path='/' element={<Landing />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/signup' element={<Signup />} />
+      </Routes>
+    </Router>
   );
 }
 
