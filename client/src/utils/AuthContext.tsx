c6572acea818bc67d26a79cb6079a2bc08d3b729
Puentebravo <puentematos@gmail.com>
2023-12-06 12:39:48 -0500

    added context API for auth tracking

---
+++ client/src/utils/AuthContext.tsx
@@ -0,0 +1,41 @@
+
+import React, { FC, createContext, useContext, useState } from "react";
+import {AuthContextType, authObject} from "../@types/client"
+
+interface Props {
+    children: React.ReactNode
+}
+
+export const AuthContext = createContext<AuthContextType | null>(null);
+
+export const useAuthContext = () => useContext(AuthContext) 
+
+export const AuthProvider: React.FC<Props> = ({ children }) => {
+   
+    const [loggedIn, setLoggedIn] = useState<authObject>({
+        loggedIn: false,
+        userId: ""
+    })
+
+    const saveAuth = (id: string) => {
+        setLoggedIn({
+            loggedIn: true,
+            userId: id
+        })
+    }
+
+    const handleLogout = () => {
+        setLoggedIn({
+            loggedIn: false,
+            userId: ""
+        })
+    }
+
+    return (
+        <AuthContext.Provider value={{loggedIn, saveAuth, handleLogout}}>
+            {children}
+        </AuthContext.Provider>
+    )
+
+
+}
