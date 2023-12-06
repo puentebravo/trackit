c042c144be4e5b3d4ea88affc6f6c3535deefb4d
Puentebravo <puentematos@gmail.com>
2023-12-06 13:55:13 -0500

    Added authcontext to brandbar

--- client/src/components/brandBar.tsx
+++ client/src/components/brandBar.tsx
@@ -1,10 +1,29 @@
 import React from "react";
+import { AuthContextType } from "../@types/client";
+import { useAuthContext } from "../utils/AuthContext";
+import { useNavigate } from "react-router-dom";
+
 
 function BrandBar() {
+
+    const {loggedIn,  handleLogout } = useAuthContext() as AuthContextType
+    
+    const navigate = useNavigate()
+
+    const signOut = () => {
+        handleLogout()
+        navigate("/login")
+    }
 
     return (
         <header id="brandBar">
             <h1 id="brandHeader">.solid</h1>
+            {loggedIn.loggedIn ?
+
+                <button id="logOutBtn" onClick={signOut}> Log Out </button>
+                :
+                <></>
+            }
         </header>
     )
 }
