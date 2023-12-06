b7803bec69f11554a13b94a5a4e3c2708327f372
Puentebravo <puentematos@gmail.com>
2023-12-06 12:40:53 -0500

    Added auth context provider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-
+import { AuthProvider } from './utils/AuthContext';
 import './App.css';
 import Landing from './pages/landing';
 import Login from './pages/login';
@@ -9,14 +9,16 @@ import Signup from './pages/signup';
 
 function App() {
   return (
-    <Router basename={`${process.env.PUBLIC_URL}`}>
-      <Routes>
-        <Route path='/' element={<Landing />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/home' element={<Home />} />
-        <Route path='/signup' element={<Signup />} />
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router basename={`${process.env.PUBLIC_URL}`}>
+        <Routes>
+          <Route path='/' element={<Landing />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/home' element={<Home />} />
+          <Route path='/signup' element={<Signup />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
