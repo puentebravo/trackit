111b9ef78836c4025a4ae71fbd88b0a1f848f837
Puentebravo <puentematos@gmail.com>
2023-12-10 21:32:59 -0500

    Refactored submit route to properly handle workout data

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -1,34 +1,64 @@
 import React, { useState } from "react";
 import BrandBar from "../../components/brandBar";
 import { useFormik } from "formik"
+import { workoutObject, AuthContextType } from "../../@types/client";
+import { useAuthContext } from "../../utils/AuthContext";
+import { useNavigate } from "react-router-dom";
 import "./home.css"
 import ProgressBar from "../../components/progressBar";
 
 function Home() {
-    const [workout, setWorkout] = useState<String>("")
+    const [workout, setWorkout] = useState<workoutObject>({
+        workout: "",
+        length: ""
+    })
+
+    const navigate = useNavigate()
+
+    const { loggedIn } = useAuthContext() as AuthContextType
 
     const [loading, setLoading] = useState<Boolean>(false)
 
     const [submitted, setSubmitted] = useState<Boolean>(false)
 
-    const repForm = useFormik({
-        initialValues: {
-            reps: ""
-        },
-        onSubmit: (values) => {
-            console.log(values)
-            fetch("/api/rep", {
-                method: "POST",
-                body: JSON.stringify(values),
-                headers: {
-                    "Content-Type": "application/json"
-                }
-            }).then(response => {
-                console.log(response)
+    // const repForm = useFormik({
+    //     initialValues: {
+    //         reps: ""
+    //     },
+    //     onSubmit: (values) => {
+    //         console.log(values)
+    //         fetch("/api/rep", {
+    //             method: "POST",
+    //             body: JSON.stringify({...workout, values}),
+    //             headers: {
+    //                 "Content-Type": "application/json"
+    //             }
+    //         }).then(response => {
+    //             console.log(response)
 
-            })
-        }
-    })
+    //         })
+    //     }
+    // })
+
+    const userId = loggedIn.userId
+
+    const handleSubmit = () => {
+        fetch("/api/rep", {
+            method: "POST",
+            body: JSON.stringify({ ...workout, userId }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }).then( response => {
+            return response.json()
+        }).then(data => {
+            console.log(data)
+            navigate("/home")
+        })
+
+
+
+    }
 
     const minForm = useFormik({
         initialValues: {
@@ -42,7 +72,12 @@ function Home() {
                 .then(response => response.json())
                 .then(data => {
                     setLoading(false)
-                    setWorkout(data.workout)
+                    setWorkout({
+                        workout: data.workout,
+                        length: minutes
+                    })
+
+                    console.log(data)
 
                 })
         }
@@ -79,21 +114,21 @@ function Home() {
                 <ProgressBar />
             </section>
 
-            <section className={workout ? "formContainer" : "formContainer hidden"}>
-                <h3 id="repsHeader">{`Do ${workout}`}</h3>
-                <form id="repsForm" onSubmit={repForm.handleSubmit}>
-                    <input
-                        type="text"
-                        name="reps"
-                        id="reps"
-                        className="formField"
-                        onChange={repForm.handleChange}
-                        value={repForm.values.reps}
-                        placeholder="How many did you do?"
-                    />
-                    <button type="submit" className="submitBtn">Send it</button>
+            <section className={workout.workout ? "formContainer" : "formContainer hidden"}>
+                <h3 id="repsHeader">{`Do ${workout.workout}`}</h3>
+                {/* <form id="repsForm" onSubmit={repForm.handleSubmit}>
+                        <input
+                            type="text"
+                            name="reps"
+                            id="reps"
+                            className="formField"
+                            onChange={repForm.handleChange}
+                            value={repForm.values.reps}
+                            placeholder="How many did you do?"
+                        /> */}
+                <button type="button" className="submitBtn" onClick={handleSubmit}>I did it!</button>
 
-                </form>
+                {/* </form> */}
             </section>
 
         </>
