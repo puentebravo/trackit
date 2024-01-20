ea32528b994405c01f55fc44d2d79a7ce3421ac0
Puentebravo <puentematos@gmail.com>
2024-01-20 13:55:23 -0500

    Added countdown component

---
+++ client/src/components/setTimer.tsx
@@ -0,0 +1,45 @@
+import React, {useState, useEffect} from "react";
+
+
+interface timerProps {
+    minutes: number
+}
+
+function SetTimer(props: timerProps) {
+
+    const [seconds, setSeconds] = useState<number>(props.minutes * 60)
+
+
+    
+    useEffect(() => {
+
+        if (seconds <= 0) {
+            return;
+        }
+        setTimeout(()=> {
+            setSeconds(seconds - 1)
+        }, 1000)
+    }, [seconds])
+    
+      
+    
+
+
+    const formatTime = (timeInSeconds: number) => {
+        let minuteForm = Math.floor(timeInSeconds / 60).toString()
+        let secForm = (timeInSeconds % 60).toString()
+
+        if (parseInt(secForm) < 10) {
+            secForm.padStart(2, "0")
+        }
+
+        return `${minuteForm}:${secForm}`
+
+    }
+
+    return (
+        <h2 className="text-bubble" id="timerText">{formatTime(seconds)}</h2>
+    )
+}
+
+export default SetTimer
