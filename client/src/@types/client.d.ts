b8011aef25ed23f15e76edd446327b878bd241e0
Puentebravo <puentematos@gmail.com>
2024-01-29 21:34:05 -0500

    Added current workout type

--- client/src/@types/client.d.ts
+++ client/src/@types/client.d.ts
@@ -1,16 +1,21 @@
-export interface authObject {
-    loggedIn: boolean,
-    userId: string
-}
+export type AuthContextType = {
+  loggedIn: authObject;
+  saveAuth: (id: string) => void;
+  handleLogout: () => void;
+};
 
-export type AuthContextType =  {
-    loggedIn: authObject,
-    saveAuth: (id: string) => void
-    handleLogout: () => void
+export interface authObject {
+  loggedIn: boolean;
+  userId: string;
 }
 
 export interface workoutObject {
-    workout: string,
-    length: string,
-    instructions: [string]
+  workout: string;
+  length: string;
+  instructions: [string];
+}
+
+export interface currentWorkoutObject {
+  workout: string;
+  instructions: [string];
 }
