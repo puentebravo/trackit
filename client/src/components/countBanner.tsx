7d24ef7142894dc415401ac9d9349f7aa9e08732
Puentebravo <puentematos@gmail.com>
2024-01-29 21:35:22 -0500

    updated countBanner structure

--- client/src/components/countBanner.tsx
+++ client/src/components/countBanner.tsx
@@ -4,11 +4,10 @@ import React from "react";
 function CountBanner() {
     return (
         <section id="sweatHeader">
-            <h2 className="text-bubble">#</h2>
-            <aside id="sweatColumnText">
-                <p className="text-pink">How many</p>
-                <p className="text-pink" id="sweatTextRight">did you do?</p>
-            </aside>
+
+            <p className="text-pink" id="sweatTextLeft">How many</p>
+            <p className="text-pink" id="sweatTextRight">did you do?</p>
+
         </section>
     )
 }
