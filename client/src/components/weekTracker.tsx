fffc909f8d01ec0729eaa24dca330daeee0171f0
Puentebravo <puentematos@gmail.com>
2024-02-26 18:56:16 -0500

    Added logic to determine date

--- client/src/components/weekTracker.tsx
+++ client/src/components/weekTracker.tsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 
-function WeekTracker() {
+interface todayDate {
+    day: number
+}
+
+function WeekTracker(props: todayDate) {
 
     const [weeklyMetrics, setWeeklyMetrics] = useState([0, 0, 0, 0, 0])
-
-    console.log(weeklyMetrics)
 
     useEffect(() => {
 
@@ -24,7 +26,6 @@ function WeekTracker() {
     }, [])
 
     const calculateColor = (minutes: number) => {
-        console.log(minutes)
         let colorClass;
         if (minutes > 0 && minutes <= 5) {
             colorClass = "back-light"
@@ -38,28 +39,36 @@ function WeekTracker() {
         return colorClass
     }
 
+    const isToday = (currentDay: number, boxDay: number) => {
+        if (currentDay === boxDay) {
+            return "today"
+        } else {
+            return ""
+        }
+    }
+
     return (
         <section id="weekTracker">
 
             <div className="weekday-box">
                 <p className="text-fade">M</p>
-                <div className={`color-box ${calculateColor(weeklyMetrics[0])}`}></div>
+                <div className={`color-box ${calculateColor(weeklyMetrics[0])} ${isToday(1, props.day)}`}></div>
             </div>
             <div className="weekday-box">
                 <p className="text-fade">T</p>
-                <div className={`color-box ${calculateColor(weeklyMetrics[1])}`}></div>
+                <div className={`color-box ${calculateColor(weeklyMetrics[1])} ${isToday(2, props.day)}`}></div>
             </div>
             <div className="weekday-box">
                 <p className="text-fade">W</p>
-                <div className={`color-box ${calculateColor(weeklyMetrics[2])}`}></div>
+                <div className={`color-box ${calculateColor(weeklyMetrics[2])} ${isToday(3, props.day)}`}></div>
             </div>
             <div className="weekday-box">
                 <p className="text-fade">Th</p>
-                <div className={`color-box ${calculateColor(weeklyMetrics[3])}`}></div>
+                <div className={`color-box ${calculateColor(weeklyMetrics[3])} ${isToday(4, props.day)}`}></div>
             </div>
             <div className="weekday-box">
                 <p className="text-fade">F</p>
-                <div className={`color-box ${calculateColor(weeklyMetrics[4])}`}></div>
+                <div className={`color-box ${calculateColor(weeklyMetrics[4])} ${isToday(5, props.day)}`}></div>
             </div>
 
 
